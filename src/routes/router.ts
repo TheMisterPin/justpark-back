9536bb3e7a38f0f031a5be1053b2437dc6d27245
TheMisterPin <137160089+TheMisterPin@users.noreply.github.com>
2024-05-10 14:22:23 -0700

    changed to give homepage

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -2,6 +2,7 @@ import parkingRoutes from './parkingRoutes'
 import wardenRoutes from './wardenRoutes'
 import authRoutes from './authRoutes'
 import userRoutes from './userRoutes'
+import parkingSessionRouter from './parkingSessionRouter'
 import express from 'express'
 
 const router = express.Router()
@@ -10,5 +11,9 @@ router.use('/parkings', parkingRoutes)
 router.use('/auth', authRoutes)
 router.use('/users', userRoutes)
 router.use('/wardens', wardenRoutes)
+router.use('/session', parkingSessionRouter)
+router.get('/', (req, res) => {
+  res.status(200).json({ message: 'JustPark backend api' })
+})
 
 export default router
