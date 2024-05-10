db8ab12201b458dde5e2ea885d11695168dd1032
TheMisterPin <137160089+TheMisterPin@users.noreply.github.com>
2024-05-10 14:26:36 -0700

    changes for router2

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,7 @@
-import express from "express";
-import http from "http";
-import cors from "cors";
-import bodyParser from "body-parser";
-import cookieParser from "cookie-parser";
-import compression from "compression";
-import router from "./routes/router";
+import app from "./routes/router";
 
-const app = express();
-app.use(cors({ credentials: true }));
+const PORT = process.env.PORT
 
-app.use(compression());
-app.use(cookieParser());
-app.use(bodyParser.json());
-
-app.use("/", router);
-
-const server = http.createServer(app);
-const port = process.env.PORT;
-server.listen(port, () => {
-  console.log("Server Running on port " + port);
-});
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
--- src/routes/router.ts
+++ src/routes/router.ts
@@ -5,15 +5,19 @@ import userRoutes from './userRoutes'
 import parkingSessionRouter from './parkingSessionRouter'
 import express from 'express'
 
-const router = express.Router()
 
-router.use('/parkings', parkingRoutes)
-router.use('/auth', authRoutes)
-router.use('/users', userRoutes)
-router.use('/wardens', wardenRoutes)
-router.use('/session', parkingSessionRouter)
-router.get('/', (req, res) => {
+const cors = require('cors')
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+app.use('/parkings', parkingRoutes)
+app.use('/auth', authRoutes)
+app.use('/users', userRoutes)
+app.use('/wardens', wardenRoutes)
+app.use('/session', parkingSessionRouter)
+app.get('/', (req, res) => {
   res.status(200).json({ message: 'JustPark backend api' })
 })
 
-export default router
+export default app
